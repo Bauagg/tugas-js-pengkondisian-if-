d8954b1373e211ab1975ff2d9ab4416d2a443cce
A. Mambaus Sholihin <bauagg87@gmail.com>
2023-03-05 19:19:12 +0700

    tugas js

---
+++ tugas css/script.js
@@ -0,0 +1,14 @@
+let baus = prompt(`masukan nilai anda 1 - 100 ?`);
+if (baus <= 20) {
+    alert(`nilai anda adalah = E`)
+} else if (baus <= 40) {
+    alert(`nilai anda adalah = D`)
+} else if (baus <= 60) {
+    alert(`nilai anda adalah = C`)
+} else if (baus <= 80) {
+    alert(`nilai anda adalah = B`)
+} else if (baus <= 100) {
+    alert(`nilai anda adalah = A`)
+} else {
+    alert(`maaf yang anda masukan salah`)
+}
